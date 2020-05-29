c3f6ad3a31c81e588ff8cb7ce62b4f8fe09999b9
Jeniua <64768209+Jeniua@users.noreply.github.com>
2020-05-29 12:51:33 +0300

    Revert "Added the ability to search for the answer entered intent(questions)"
    This reverts commit bac8d6fd032e05e96dc068fb804596696370b3af.

--- Copy-paste.py
+++ Copy-paste.py
@@ -1,70 +1,34 @@
 BOT_CONFIG = {
     'intents': {
-        'Hello' :{
+        'hello' :{
             'questions': ['Hello','Good day'],
-            'responses': ['Hi', 'Nice to meet you']
+            'answers': ['Hi', 'Nice to meet you']
         },
         'bye':{
             'questions': ['Bye','Good luck'],
-            'responses': ['Bye bye', 'see you']
+            'answers': ['Bye bye', 'see you']
         },
         'whatcanyoudo':{
             'questions':['What you can', 'Tell what can you do'],
-            'responses':['I can answer on questions. Just ask something']
+            'answers':['I can answer on questions. Just ask something']
         },
         'name':{
             'questions':['What\'s your name','Name?'],
-            'responses':['My name is Bot']
+            'answer':['My name is Bot']
         },
         'weather':{
             'questions':['What\'s the weather now'],
-            'responses':['Wonderfull weather','Good for a walk']
-        },
-        'thanks':{
-            'questions':['Thank you','10q','thanks'],
-            'responses':['Thank you too']
-        },
+            'answer':['Wonderfull weather','Good for a walk']
+        }
     },
-    'failure_phrase':[
+    'default_answers':[
         'Don\'t understand you',
         'Sorry,repeat please',
         'Reformulate',
     ]
 }
-
-import random
-def get_intent(text):
-    intents = BOT_CONFIG['intents']
-    for intent,value in intents.items():
-        for example in value['questions']:
-            if text == example:
-                return intent
-
-def get_response_by_intent(intent):
-    response = BOT_CONFIG['intents'][intent]['responses']
-    return random.choice(response)
-
-def get_failure_phrase():
-    phrases = BOT_CONFIG['failure_phrase']
-    return random.choice(phrases)
-
 def generate_answer(text):
-    # NLU
-    global answer
-    intent = get_intent(text)
-
-    # Make answer
-
-    #by script
-    if intent:
-        response = get_response_by_intent(intent)
-        return response
-    #use generative model
-    #TODO
-
-    #use stub
-    failure_phrase = get_failure_phrase()
-    return failure_phrase
+    return text
 while True:
     text = input('Enter your question: ')
     answer = generate_answer(text)
